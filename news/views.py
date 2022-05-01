bf98c87925178ce4daff342591ce8e332b7c24e8
polina <123@example.com>
2022-05-01 23:50:40 +0300

    Add changes and static

--- news/views.py
+++ news/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from news.models import Category, News
 
 # Create your views here.
+def main_page(request):
+    categories_list = Category.objects.all()
+    news_list = News.objects.order_by('-id').all()
+    context = {'categories_list': categories_list, 'news_list': news_list}
+    return render(request, 'index.html', context=context)
+
+
