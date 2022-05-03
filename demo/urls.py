a738187a9037f98f37c4ca758e00463aae30c1d8
polina <123@example.com>
2022-05-03 18:14:23 +0300

    Add new changes and static

--- demo/urls.py
+++ demo/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from news import views
 
@@ -22,5 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_page, name='home'),
     path('category_<str:cat_name>/', views.category, name='category_name'),
+
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/models.py
+++ news/models.py
@@ -25,6 +25,7 @@ class News(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tags, blank=True)
+    image = models.ImageField(upload_to='news/images/', blank=True, null=True)
 
     def __str__(self):
         return f'{self.title}: {self.text}'
