b09cd115c81f2a9487a3a05f2a55649a7b49512b
alex <kopytko@ukrtelebud.com.ua>
2022-05-04 01:07:31 +0300

    update settings

--- demo/settings.py
+++ demo/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django',
         'USER': 'django',
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
