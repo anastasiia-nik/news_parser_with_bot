0f2c718799838d5fc9ac3f2edab99f2f1490f34c
alex <kopytko@ukrtelebud.com.ua>
2022-05-04 02:21:58 +0300

    update settings

--- demo/settings.py
+++ demo/settings.py
@@ -15,8 +15,9 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
