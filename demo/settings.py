e75a649631ce219e0515516766e2b8414c8ad500
alex <kopytko@ukrtelebud.com.ua>
2022-05-04 14:29:56 +0300

    update settings

--- demo/settings.py
+++ demo/settings.py
@@ -84,14 +84,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'second': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'django',
+    #     'USER': 'django',
+    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+    #     'HOST': 'localhost',
+    #     'PORT': 5432,
+    # }
 }
 
 # Password validation
