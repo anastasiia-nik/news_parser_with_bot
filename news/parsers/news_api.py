7d9edafcb6ee34f53fff5ac777091d1c62f9fdb1
Ana <an.gordievich@gmail.com>
2022-05-04 23:28:34 +0300

    add api parser

--- news/parsers/news_api.py
+++ news/parsers/news_api.py
@@ -1,46 +1,37 @@
+import requests
+
 from base import NewsParser
 from bs4 import BeautifulSoup
-from newsapi import NewsApiClient
 
+BASE_URL = 'https://www.pravda.com.ua/news/'
 
-#81941d1679c2475388fe5337be537783
-
-# https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=81941d1679c2475388fe5337be537783
-
-
-
-newsapi = NewsApiClient(api_key='81941d1679c2475388fe5337be537783')
-
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us')
-
-# /v2/everything
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
-
-# /v2/top-headlines/sources
-sources = newsapi.get_sources()
-
-print(sources)
 
 
 
 class Upravda(NewsParser):
-    pass
+
+    @staticmethod
+    def collect_all_news() -> list[str]:
+        resp = requests.get(BASE_URL)
+        bs = BeautifulSoup(resp.text, 'lxml')
+        news_on_page = bs.find_all('div', class_='article_news_list')
+        news_list = []
+        for news in news_on_page:
+            temp_link = news.find('a').attrs['href']
+            if 'https' not in temp_link:
+                news_list.append('https://www.pravda.com.ua/' + temp_link)
+        return news_list
+
+    @staticmethod
+    def parse(link):
+        title - h1
+        author - post_author.text
+        data - post time
+        photo post_photo_news_img scr
 
 
 
 
 
-
-
+test = Upravda()
+test.parse_all_news()
